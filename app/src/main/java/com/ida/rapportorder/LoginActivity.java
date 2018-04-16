acb9eebd07270cb9decf0cee8a7cd7b28fe96b8d
Ida Lundgren <idlu1201@student.miun.se>
2018-04-16 14:21:37 +0200

    Kopplat upp mot firebase och firestore.

--- app/src/main/java/com/ida/rapportorder/LoginActivity.java
+++ app/src/main/java/com/ida/rapportorder/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.ida.rapportorder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+    }
+
+
+    //Executes when register button is clicked
+    public void registerNewUser (View v){
+        Intent newIntent = new Intent(this, RegisterActivity.class);
+        finish();
+        startActivity(newIntent);
     }
 }
--- app/src/main/java/com/ida/rapportorder/RegisterActivity.java
+++ app/src/main/java/com/ida/rapportorder/RegisterActivity.java
@@ -2,12 +2,43 @@ package com.ida.rapportorder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    //UI references
+    private EditText mFirstnameView;
+    private EditText mLastnameView;
+    private EditText mCellphoneView;
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private EditText mConfirmPasswordView;
+    private EditText mUserRoleView;
+
+    //Firebase instance variables
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFirstnameView = findViewById(R.id.fname);
+        mLastnameView = findViewById(R.id.lname);
+        mCellphoneView = findViewById(R.id.cellphone);
+        mEmailView = findViewById(R.id.email);
+        mPasswordView = findViewById(R.id.password);
+        mConfirmPasswordView = findViewById(R.id.confirmpassword);
+        mUserRoleView = findViewById(R.id.userRole);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+    private boolean isEmailValid(String email){
+        return email.contains("@");
+    }
+    private boolean isPasswordValid(String password){
+        String confirmPassword = mConfirmPasswordView.getText().toString();
+        return confirmPassword.equals(password) && password.length() > 4;
     }
 }
