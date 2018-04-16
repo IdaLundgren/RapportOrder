ecedd9bbd4295fc634a0a93e6c33b2633a5ce655
Ida Lundgren <idlu1201@student.miun.se>
2018-04-16 14:24:29 +0200

    lagt till google-service json och en klass

---
+++ app/src/main/java/com/ida/rapportorder/UserModel.java
@@ -0,0 +1,13 @@
+package com.ida.rapportorder;
+
+/**
+ * Created by Ida on 2018-04-16.
+ */
+
+public class UserModel {
+    private String mFirstname;
+    private String mLastname;
+    private String mCellphone;
+    private String mEmail;
+    private String mPassword;
+}
